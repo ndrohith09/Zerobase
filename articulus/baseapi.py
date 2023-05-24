5eacf2c211402c6c03a5e0f39f8922f1bc2341f7
Rohith ND <73429989+ndrohith09@users.noreply.github.com>
2023-05-24 13:11:33 +0530

    Update baseapi.py

--- articulus/baseapi.py
+++ articulus/baseapi.py
@@ -25,116 +25,20 @@ except (Exception, psycopg2.DatabaseError) as error:
     print(error)  
 
 #*main
-def main():
-    cursor.execute('CREATE TABLE IF NOT EXISTS Birds (id SERIAL PRIMARY KEY, name VARCHAR(255), breed VARCHAR(255))') 
-    conn.commit()
-    cursor.execute('CREATE TABLE IF NOT EXISTS Animals (id SERIAL PRIMARY KEY, name VARCHAR(255), country VARCHAR(255))') 
-    conn.commit() 
+def main(): 
     print("Table created successfully")
 
 #*Dataclasses
-@strawberry.type
-class Birds:
-    id: str
-    name: str
-    breed: str
-
-@strawberry.type
-class Animals:
-    id: str
-    name: str
-    country: str
-
  
 
 @strawberry.type
 class Query:
-    #*graphquery
-    @strawberry.field
-    def all_birds(self) -> typing.List[Birds]:
-        cursor.execute("SELECT * FROM Birds")
-        lst = cursor.fetchall()
-        birds = []
-        for i in lst:
-            birds.append(Birds(id=i[0], name=i[1], breed=i[2]))
-        return birds
-
-    @strawberry.field
-    def get_birds(self, id: str) -> Birds:
-        cursor.execute("SELECT * FROM Birds WHERE id = %s", (id,))
-        lst = cursor.fetchone()
-        return Birds(id=lst[0], name=lst[1], breed=lst[2])
-    
-    @strawberry.field
-    def all_animals(self) -> typing.List[Animals]:
-        cursor.execute("SELECT * FROM Animals")
-        lst = cursor.fetchall()
-        animals = []
-        for i in lst:
-            animals.append(Animals(id=i[0], name=i[1], country=i[2]))
-        return animals
-
-    @strawberry.field
-    def get_animals(self, id: str) -> Animals:
-        cursor.execute("SELECT * FROM Animals WHERE id = %s", (id,))
-        lst = cursor.fetchone()
-        return Animals(id=lst[0], name=lst[1], country=lst[2])
-         
+    #*graphquery     
     
 
 @strawberry.type
 class Mutation:
-    #*graphmutation
-    @strawberry.mutation
-    def create_birds(self, name: str, breed: str) -> Birds:
-        cursor.execute("INSERT INTO Birds (name, breed) VALUES (%s, %s) RETURNING id", (name, breed))
-        conn.commit()
-        birds_id = cursor.fetchone()[0]
-        return Birds(id=birds,name=name, breed=breed)
-    
-    @strawberry.mutation
-    def update_birds(self, id: str, name: str, breed: str) -> Birds:
-        
-        cursor.execute("UPDATE Birds SET name=%s, breed=%s WHERE id = %s", (name, breed, id))
-        conn.commit()
-        return Birds(id=id,name=name, breed=breed)
-    
-    @strawberry.mutation
-    def delete_birds(self, id: str) -> Birds:
-        cursor.execute("SELECT * FROM Birds WHERE id = %s", (id,))
-        lst = cursor.fetchone()
-        if lst is None:
-            return Birds(name='No Data Found', breed='No Data Found')
-
-        cursor.execute("DELETE FROM Birds WHERE id = %s", (id,))
-        conn.commit()
-        return Birds(id=id,id=lst[0], name=lst[1], breed=lst[2])
-    
-    @strawberry.mutation
-    def create_animals(self, name: str, country: str) -> Animals:
-        cursor.execute("INSERT INTO Animals (name, country) VALUES (%s, %s) RETURNING id", (name, country))
-        conn.commit()
-        animals_id = cursor.fetchone()[0]
-        return Animals(id=animals,name=name, country=country)
-    
-    @strawberry.mutation
-    def update_animals(self, id: str, name: str, country: str) -> Animals:
-        
-        cursor.execute("UPDATE Animals SET name=%s, country=%s WHERE id = %s", (name, country, id))
-        conn.commit()
-        return Animals(id=id,name=name, country=country)
-    
-    @strawberry.mutation
-    def delete_animals(self, id: str) -> Animals:
-        cursor.execute("SELECT * FROM Animals WHERE id = %s", (id,))
-        lst = cursor.fetchone()
-        if lst is None:
-            return Animals(name='No Data Found', country='No Data Found')
-
-        cursor.execute("DELETE FROM Animals WHERE id = %s", (id,))
-        conn.commit()
-        return Animals(id=id,id=lst[0], name=lst[1], country=lst[2])
-     
+    #*graphmutation 
 
 schema = strawberry.Schema(Query , Mutation)
 
