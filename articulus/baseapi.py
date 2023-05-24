2a8f1891a9697173e61ba405eb2fdef6e7b0e76d
Rohith ND <73429989+ndrohith09@users.noreply.github.com>
2023-05-24 13:10:28 +0530

    Initialised graphql Birds functions

--- articulus/baseapi.py
+++ articulus/baseapi.py
@@ -26,11 +26,19 @@ except (Exception, psycopg2.DatabaseError) as error:
 
 #*main
 def main():
+    cursor.execute('CREATE TABLE IF NOT EXISTS Birds (id SERIAL PRIMARY KEY, name VARCHAR(255), breed VARCHAR(255))') 
+    conn.commit()
     cursor.execute('CREATE TABLE IF NOT EXISTS Animals (id SERIAL PRIMARY KEY, name VARCHAR(255), country VARCHAR(255))') 
     conn.commit() 
     print("Table created successfully")
 
 #*Dataclasses
+@strawberry.type
+class Birds:
+    id: str
+    name: str
+    breed: str
+
 @strawberry.type
 class Animals:
     id: str
@@ -42,6 +50,21 @@ class Animals:
 @strawberry.type
 class Query:
     #*graphquery
+    @strawberry.field
+    def all_birds(self) -> typing.List[Birds]:
+        cursor.execute("SELECT * FROM Birds")
+        lst = cursor.fetchall()
+        birds = []
+        for i in lst:
+            birds.append(Birds(id=i[0], name=i[1], breed=i[2]))
+        return birds
+
+    @strawberry.field
+    def get_birds(self, id: str) -> Birds:
+        cursor.execute("SELECT * FROM Birds WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        return Birds(id=lst[0], name=lst[1], breed=lst[2])
+    
     @strawberry.field
     def all_animals(self) -> typing.List[Animals]:
         cursor.execute("SELECT * FROM Animals")
@@ -62,6 +85,31 @@ class Query:
 @strawberry.type
 class Mutation:
     #*graphmutation
+    @strawberry.mutation
+    def create_birds(self, name: str, breed: str) -> Birds:
+        cursor.execute("INSERT INTO Birds (name, breed) VALUES (%s, %s) RETURNING id", (name, breed))
+        conn.commit()
+        birds_id = cursor.fetchone()[0]
+        return Birds(id=birds,name=name, breed=breed)
+    
+    @strawberry.mutation
+    def update_birds(self, id: str, name: str, breed: str) -> Birds:
+        
+        cursor.execute("UPDATE Birds SET name=%s, breed=%s WHERE id = %s", (name, breed, id))
+        conn.commit()
+        return Birds(id=id,name=name, breed=breed)
+    
+    @strawberry.mutation
+    def delete_birds(self, id: str) -> Birds:
+        cursor.execute("SELECT * FROM Birds WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        if lst is None:
+            return Birds(name='No Data Found', breed='No Data Found')
+
+        cursor.execute("DELETE FROM Birds WHERE id = %s", (id,))
+        conn.commit()
+        return Birds(id=id,id=lst[0], name=lst[1], breed=lst[2])
+    
     @strawberry.mutation
     def create_animals(self, name: str, country: str) -> Animals:
         cursor.execute("INSERT INTO Animals (name, country) VALUES (%s, %s) RETURNING id", (name, country))
