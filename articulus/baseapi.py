e4d71667855d870afe6defb517ed98b27c8eb1e7
Rohith ND <73429989+ndrohith09@users.noreply.github.com>
2023-05-24 12:33:05 +0530

    Append graphql functions

--- articulus/baseapi.py
+++ articulus/baseapi.py
@@ -27,16 +27,42 @@ except (Exception, psycopg2.DatabaseError) as error:
 
     
 def main():
+    cursor.execute(CREATE TABLE IF NOT EXISTS Birds (id SERIAL PRIMARY KEY, name VARCHAR(255), family VARCHAR(255))) 
+    conn.commit()
+
     
     print("Inside main")
 
 #*Dataclasses
+@strawberry.type
+class Birds:
+    id: str
+    name: str
+    family: str
+
 
     
 @strawberry.type
 class Query:
     
-    #*graphquery    
+    
+    #*graphquery
+    @strawberry.field
+    async def all_birds(self) -> typing.List[Birds]:
+        cursor.execute("SELECT * FROM Birds")
+        lst = cursor.fetchall()
+        birds = []
+        for i in lst:
+            birds.append(Birds(id=i[0], name=i[1], family=i[2]))
+        return birds
+
+    @strawberry.field
+    async def get_birds(self, id: str) -> Birds:
+        cursor.execute("SELECT * FROM Birds WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        return Birds(id=lst[0], name=lst[1], family=lst[2])
+
+        
 
 @strawberry.type
 class Mutation:
