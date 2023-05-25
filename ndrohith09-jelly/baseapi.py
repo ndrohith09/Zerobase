9819f7e04aff8afef6851c953e863a095c0a1496
Rohith ND <73429989+ndrohith09@users.noreply.github.com>
2023-05-25 19:04:12 +0530

    Initialised graphql Fish functions

--- ndrohith09-jelly/baseapi.py
+++ ndrohith09-jelly/baseapi.py
@@ -27,12 +27,19 @@ except (Exception, psycopg2.DatabaseError) as error:
     print(error)  
 
 #*main
-def main(): 
+def main():
+    cursor.execute('CREATE TABLE IF NOT EXISTS Fish (id SERIAL PRIMARY KEY, breed VARCHAR(200))') 
+    conn.commit() 
     cursor.execute('CREATE TABLE IF NOT EXISTS Sample (id SERIAL PRIMARY KEY, word VARCHAR(255))') 
     conn.commit() 
     print("Table created successfully")
 
 #*Dataclasses
+@strawberry.type
+class Fish:
+    id: str
+    breed: str
+
 @strawberry.type
 class Sample:
     id : str 
@@ -40,7 +47,22 @@ class Sample:
 
 @strawberry.type
 class Query:
-    #*graphquery     
+    #*graphquery
+    @strawberry.field
+    def all_fish(self) -> typing.List[Fish]:
+        cursor.execute("SELECT * FROM Fish")
+        lst = cursor.fetchall()
+        fish = []
+        for i in lst:
+            fish.append(Fish(id=i[0], breed=i[1]))
+        return fish
+
+    @strawberry.field
+    def get_fish(self, id: str) -> Fish:
+        cursor.execute("SELECT * FROM Fish WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        return Fish(id=lst[0], breed=lst[1])
+         
 
     @strawberry.field
     def all_sample(self) -> typing.List[Sample]:
@@ -53,7 +75,32 @@ class Query:
 
 @strawberry.type
 class Mutation:
-    #*graphmutation 
+    #*graphmutation
+    @strawberry.mutation
+    def create_fish(self, breed: str) -> Fish:
+        cursor.execute("INSERT INTO Fish (breed) VALUES (%s) RETURNING id", (breed))
+        conn.commit()
+        fish_id = cursor.fetchone()[0]
+        return Fish(id=fish_id,breed=breed)
+    
+    @strawberry.mutation
+    def update_fish(self, id: str, breed: str) -> Fish:
+        
+        cursor.execute("UPDATE Fish SET breed=%s WHERE id = %s", (breed, id))
+        conn.commit()
+        return Fish(id=id,breed=breed)
+    
+    @strawberry.mutation
+    def delete_fish(self, id: str) -> Fish:
+        cursor.execute("SELECT * FROM Fish WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        if lst is None:
+            return Fish(id='No Data Found',breed='No Data Found')
+
+        cursor.execute("DELETE FROM Fish WHERE id = %s", (id,))
+        conn.commit()
+        return Fish(id=lst[0], breed=lst[1])
+     
 
     @strawberry.mutation
     def create_sample(self, word: str) -> Sample:
