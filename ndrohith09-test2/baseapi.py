35d7718555fca1a34ab221b89bd15d3d43f63f0f
Rohith ND <73429989+ndrohith09@users.noreply.github.com>
2023-05-25 18:07:26 +0530

    Initialised graphql Animals functions

--- ndrohith09-test2/baseapi.py
+++ ndrohith09-test2/baseapi.py
@@ -27,12 +27,20 @@ except (Exception, psycopg2.DatabaseError) as error:
     print(error)  
 
 #*main
-def main(): 
+def main():
+    cursor.execute('CREATE TABLE IF NOT EXISTS Animals (id SERIAL PRIMARY KEY, name VARCHAR(200), breed VARCHAR(200))') 
+    conn.commit() 
     cursor.execute('CREATE TABLE IF NOT EXISTS Sample (id SERIAL PRIMARY KEY, word VARCHAR(255))') 
     conn.commit() 
     print("Table created successfully")
 
 #*Dataclasses
+@strawberry.type
+class Animals:
+    id: str
+    name: str
+    breed: str
+
 @strawberry.type
 class Sample:
     id : str 
@@ -40,7 +48,22 @@ class Sample:
 
 @strawberry.type
 class Query:
-    #*graphquery     
+    #*graphquery
+    @strawberry.field
+    def all_animals(self) -> typing.List[Animals]:
+        cursor.execute("SELECT * FROM Animals")
+        lst = cursor.fetchall()
+        animals = []
+        for i in lst:
+            animals.append(Animals(id=i[0], name=i[1], breed=i[2]))
+        return animals
+
+    @strawberry.field
+    def get_animals(self, id: str) -> Animals:
+        cursor.execute("SELECT * FROM Animals WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        return Animals(id=lst[0], name=lst[1], breed=lst[2])
+         
 
     @strawberry.field
     def all_sample(self) -> typing.List[Sample]:
@@ -53,7 +76,32 @@ class Query:
 
 @strawberry.type
 class Mutation:
-    #*graphmutation 
+    #*graphmutation
+    @strawberry.mutation
+    def create_animals(self, name: str, breed: str) -> Animals:
+        cursor.execute("INSERT INTO Animals (name, breed) VALUES (%s, %s) RETURNING id", (name, breed))
+        conn.commit()
+        animals_id = cursor.fetchone()[0]
+        return Animals(id=animals_id,name=name, breed=breed)
+    
+    @strawberry.mutation
+    def update_animals(self, id: str, name: str, breed: str) -> Animals:
+        
+        cursor.execute("UPDATE Animals SET name=%s, breed=%s WHERE id = %s", (name, breed, id))
+        conn.commit()
+        return Animals(id=id,name=name, breed=breed)
+    
+    @strawberry.mutation
+    def delete_animals(self, id: str) -> Animals:
+        cursor.execute("SELECT * FROM Animals WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        if lst is None:
+            return Animals(id='No Data Found',name='No Data Found', breed='No Data Found')
+
+        cursor.execute("DELETE FROM Animals WHERE id = %s", (id,))
+        conn.commit()
+        return Animals(id=lst[0], name=lst[1], breed=lst[2])
+     
 
     @strawberry.mutation
     def create_sample(self, word: str) -> Sample:
