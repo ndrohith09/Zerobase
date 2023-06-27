24b9f3174289b634bc0c609291966d313c7e68f2
Rohith ND <73429989+ndrohith09@users.noreply.github.com>
2023-06-27 12:50:43 +0530

    Initialised graphql Employee functions

--- ndrohith09-HR management system/baseapi.py
+++ ndrohith09-HR management system/baseapi.py
@@ -36,12 +36,24 @@ except (Exception, psycopg2.DatabaseError) as error:
     print(error)  
 
 #*main
-def main(): 
+def main():
+    cursor.execute('CREATE TABLE IF NOT EXISTS Employee (id SERIAL PRIMARY KEY, e_id INT, name VARCHAR, email VARCHAR, phone INT, age INT, salary DECIMAL)') 
+    conn.commit() 
     cursor.execute('CREATE TABLE IF NOT EXISTS Sample (id SERIAL PRIMARY KEY, word VARCHAR(255))') 
     conn.commit() 
     print("Table created successfully")
 
 #*Dataclasses
+@strawberry.type
+class Employee:
+    id: str
+    e_id: str
+    name: str
+    email: str
+    phone: str
+    age: str
+    salary: str
+
 @strawberry.type
 class Sample:
     id : str 
@@ -49,7 +61,22 @@ class Sample:
 
 @strawberry.type
 class Query:
-    #*graphquery     
+    #*graphquery
+    @strawberry.field
+    def all_employee(self) -> typing.List[Employee]:
+        cursor.execute("SELECT * FROM Employee")
+        lst = cursor.fetchall()
+        employee = []
+        for i in lst:
+            employee.append(Employee(id=i[0], e_id=i[1], name=i[2], email=i[3], phone=i[4], age=i[5], salary=i[6]))
+        return employee
+
+    @strawberry.field
+    def get_employee(self, id: str) -> Employee:
+        cursor.execute("SELECT * FROM Employee WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        return Employee(id=lst[0], e_id=lst[1], name=lst[2], email=lst[3], phone=lst[4], age=lst[5], salary=lst[6])
+         
 
     @strawberry.field
     def all_sample(self) -> typing.List[Sample]:
@@ -62,7 +89,32 @@ class Query:
 
 @strawberry.type
 class Mutation:
-    #*graphmutation 
+    #*graphmutation
+    @strawberry.mutation
+    def create_employee(self, e_id: str, name: str, email: str, phone: str, age: str, salary: str) -> Employee:
+        cursor.execute("INSERT INTO Employee (e_id, name, email, phone, age, salary) VALUES (%s, %s, %s, %s, %s, %s) RETURNING id", (e_id, name, email, phone, age, salary))
+        conn.commit()
+        employee_id = cursor.fetchone()[0]
+        return Employee(id=employee_id,e_id=e_id, name=name, email=email, phone=phone, age=age, salary=salary)
+    
+    @strawberry.mutation
+    def update_employee(self, id: str, e_id: str, name: str, email: str, phone: str, age: str, salary: str) -> Employee:
+        
+        cursor.execute("UPDATE Employee SET e_id=%s, name=%s, email=%s, phone=%s, age=%s, salary=%s WHERE id = %s", (e_id, name, email, phone, age, salary, id))
+        conn.commit()
+        return Employee(id=id,e_id=e_id, name=name, email=email, phone=phone, age=age, salary=salary)
+    
+    @strawberry.mutation
+    def delete_employee(self, id: str) -> Employee:
+        cursor.execute("SELECT * FROM Employee WHERE id = %s", (id,))
+        lst = cursor.fetchone()
+        if lst is None:
+            return Employee(id='No Data Found',e_id='No Data Found', name='No Data Found', email='No Data Found', phone='No Data Found', age='No Data Found', salary='No Data Found')
+
+        cursor.execute("DELETE FROM Employee WHERE id = %s", (id,))
+        conn.commit()
+        return Employee(id=lst[0], e_id=lst[1], name=lst[2], email=lst[3], phone=lst[4], age=lst[5], salary=lst[6])
+     
 
     @strawberry.mutation
     def create_sample(self, word: str) -> Sample:
